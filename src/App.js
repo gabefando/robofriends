eed797781fe7bc0002c539be2e548bafb5ac7135
gabefando <gabe.fonseca.04@gmail.com>
2022-08-20 05:46:48 -0400

    added all to app.js, added title and search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import CardList from "./components/CardList";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
     return (
-        <CardList />
+        <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList />
+        </div>
     )
 }
+
+export default App;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <input className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="Search Robots" />
+    )
+}
+
+export default SearchBox;
